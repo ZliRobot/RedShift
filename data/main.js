71bab5aa8a730515c3f26a1385f8a4ac09adea5e
ZliRobot <dusanradovan@gmail.com>
2022-12-30 21:14:31 +0100

    Web app fixes and cleanup

--- data/main.js
+++ data/main.js
@@ -71,32 +71,6 @@
       console.error('Error during service worker registration:', e);
     });
   }
-
-  // Your custom JavaScript goes here
-	var CACHE_NAME = 'my-offline-cache';
-	var urlsToCache = [
-	'/'
-	];
-
-	self.addEventListener('install', function(event) {
-		event.waitUntil(
-			caches.open(CACHE_NAME)
-			.then(function(cache) {
-				return cache.addAll(urlsToCache);
-			})
-		);
-	});	
-	
-	self.addEventListener('fetch', function(event) {
-	  event.respondWith(
-		fetch(event.request).catch(function() {
-		  caches.match(event.request).then(function(response) {
-			return response;
-		  }
-		);
-	  );
-	});
-	
 	
 	// Custom JS end
 })();
